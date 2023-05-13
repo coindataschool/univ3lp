ad2fa9f982a33d696e594b3f37e4f8776376243c
Coin Data School <coindataschool@gmail.com>
2023-05-13 11:11:32 +0900

    add transfer_gas_eth

--- scripts/analyze-closed-positions/Python/04-aggregate-ETH-pairs.py
+++ scripts/analyze-closed-positions/Python/04-aggregate-ETH-pairs.py
@@ -10,6 +10,7 @@ capital_usdc = 10000        # How many USDC did you use to LP?
 capital_usdt = 0            # How many USDT did you use to LP?
 transfer_cost_usdc = -(0)   # non-positive, cost of on-ramp, stargate transfer
 transfer_cost_usdt = -(0)   # non-positive, cost of on-ramp, stargate transfer
+transfer_gas_eth = -(0)     # non-positive, gas on transfer starting and arrival chains
 approve_gas_eth = -(0)      # non-positive, gas for token approval 
 failed_mint_gas_eth = -(0)  # non-positive, gas for failed LP tx
 
@@ -22,7 +23,7 @@ current_eth_price = prices.loc[prices['symbol'] == 'WETH', 'price'][0]
 
 # main
 transfer_cost_usd = transfer_cost_usdc + transfer_cost_usdt
-other_gas_eth = approve_gas_eth + failed_mint_gas_eth
+other_gas_eth = transfer_gas_eth + approve_gas_eth + failed_mint_gas_eth
 aggregate_ethStable_altStable_altEth(
     wallet_name, pair, transfer_cost_usd, other_gas_eth,
     capital_eth, capital_usdc, capital_usdt, capital_alt=0,
--- scripts/analyze-closed-positions/Python/04-aggregate-MCB-pairs.py
+++ scripts/analyze-closed-positions/Python/04-aggregate-MCB-pairs.py
@@ -11,6 +11,7 @@ capital_usdc = 1000         # How many USDC did you use to LP?
 capital_usdt = 0            # How many USDT did you use to LP?
 transfer_cost_usdc = -(0)   # non-positive, cost of on-ramp, stargate transfer
 transfer_cost_usdt = -(0)   # non-positive, cost of on-ramp, stargate transfer
+transfer_gas_eth = -(0)     # non-positive, gas on transfer starting and arrival chains
 approve_gas_eth = -(0)      # non-positive, gas for token approval 
 failed_mint_gas_eth = -(0)  # non-positive, gas for failed LP tx
 
@@ -24,7 +25,7 @@ current_mcb_price = prices.loc[prices['symbol'] == 'MCB', 'price'][0]
 
 # main
 transfer_cost_usd = transfer_cost_usdc + transfer_cost_usdt
-other_gas_eth = approve_gas_eth + failed_mint_gas_eth
+other_gas_eth = transfer_gas_eth + approve_gas_eth + failed_mint_gas_eth
 aggregate_ethStable_altStable_altEth(
     wallet_name, pair, transfer_cost_usd, other_gas_eth, 
     capital_eth, capital_usdc, capital_usdt=0, capital_alt=capital_mcb, current_eth_price=current_eth_price, current_alt_price=current_mcb_price
