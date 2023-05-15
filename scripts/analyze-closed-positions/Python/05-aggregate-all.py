d2f5861b1e012235112a0458b49a9884287add3e
Coin Data School <coindataschool@gmail.com>
2023-05-15 11:11:44 +0900

    add reused eth capital input

--- scripts/analyze-closed-positions/Python/05-aggregate-all.py
+++ scripts/analyze-closed-positions/Python/05-aggregate-all.py
@@ -2,14 +2,18 @@ import pandas as pd
 import pickle 
 import os
 
-# input 
-capital_reused = 1000 # capital re-used in different LP pairs. For example,
-    # you may use $1000 USDC in a single-sided ETH-USDC position, and say price
-    # takes a round-trip and you get back your $1000 USDC after closure, and 
-    # you deploy the same $1000 USDC in a GMX-USDC pool. This $1000 USDC will
-    # be considered as re-used. Even if there's no round trip, say your $1000 
-    # USDC gets all converted to ETH, and you close and take the ETH and deploy 
-    # to the ARB-ETH pool, your $1000 USDC capital will still be re-used.
+# --- BEGIN Input --- #
+
+capital_reused_usd = 1000 # USDC or USDT capital re-used in different LP pairs. 
+    # For example, say you single-sided $1000 USDC in an ETH-USDC position, and 
+    # say price took a round-trip and you got back your $1000 USDC after closure, 
+    # and you deployed the same $1000 USDC in a GMX-USDC pool. This $1000 USDC 
+    # would be considered as re-used. Even if no round trip, say your $1000 
+    # USDC got all converted to ETH, and you closed and sent the ETH to an 
+    # ARB-ETH pool, your $1000 USDC capital would still re-used.
+capital_reused_eth = 0 # ETH capital re-used in different LP pairs.
+
+# --- END Input --- #
 
 # read data
 data_dir = '../data'
@@ -21,14 +25,20 @@ for name in os.listdir(data_dir):
 df = pd.concat(lst).sort_values('daily_fees', ascending=False)
 df.to_csv(os.path.join(data_dir, 'final-aggregate-all.csv'), index=False)
 
+# load current eth price
+with open('../data/current_prices.pickle', 'rb') as handle:
+    prices = pickle.load(handle)
+current_eth_price = prices.loc[prices['symbol'] == 'WETH', 'price'][0]
+
 # print key metrics on screen
 timediff = df['stop_time'].max() - df['start_time'].min()
 dur_days = timediff.days + timediff.seconds/3600/24
 positions = df['closed_position_cnt'].sum()
 fees_less_gas = df['total_fees_less_gas'].sum()
 pnl = df['total_pnl'].sum()
-capital = df['capital'].sum() - capital_reused # remove multiple counting of 
-    # the same capital
+# remove multiple counting of the same capital due to re-usage
+capital = df['capital'].sum() - capital_reused_usd\
+    - capital_reused_usd * current_eth_price
 print("Duration:", '{:.2f} days'.format(dur_days))
 print('At Current Prices,')
 print('  - Total Capital:', '${:.2f}'.format(capital)) 
